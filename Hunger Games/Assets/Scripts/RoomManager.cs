0d9f0a641b3bfcbb78b21a685ae8e4648d7d1f0c
Julian Nieuwenburg <111181@st-maartenscollege.nl>
2025-02-25 12:28:59 +0100

    SpawnPoints werken

--- Hunger Games/Assets/Scripts/RoomManager.cs
+++ Hunger Games/Assets/Scripts/RoomManager.cs
@@ -5,47 +5,63 @@ using Photon.Pun;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
-
     public GameObject player;
     [Space]
-    public Transform spawnPoint;
+    public Transform[] spawnPoints;
+    private List<int> availableSpawnIndices = new List<int>();
 
     [Space]
     public GameObject roomCam;
 
-    // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Connecting...");
-        
         PhotonNetwork.ConnectUsingSettings();
     }
 
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-
         Debug.Log("Connected");
-
         PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
     {
         base.OnJoinedLobby();
-
         PhotonNetwork.JoinOrCreateRoom("test", null, null);
-
         Debug.Log("We zitten nu in een room");
     }
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
+        roomCam.SetActive(false);
 
-        roomCam.SetActive(false );
+        // Vul de lijst met alle beschikbare spawn indices
+        if (PhotonNetwork.IsMasterClient)
+        {
+            for (int i = 0; i < spawnPoints.Length; i++)
+            {
+                availableSpawnIndices.Add(i);
+            }
+        }
 
-        GameObject _player = PhotonNetwork.Instantiate(this.player.name, spawnPoint.position, Quaternion.identity);
+        StartCoroutine(SpawnPlayer());
+    }
+
+    IEnumerator SpawnPlayer()
+    {
+        while (availableSpawnIndices.Count == 0)
+        {
+            yield return null;
+        }
+
+        int randomIndex = availableSpawnIndices[Random.Range(0, availableSpawnIndices.Count)];
+        availableSpawnIndices.Remove(randomIndex);
+
+        Vector3 spawnPosition = spawnPoints[randomIndex].position;
+        GameObject _player = PhotonNetwork.Instantiate(this.player.name, spawnPosition, Quaternion.identity);
         _player.GetComponent<PlayerSetup>().IsLocalPlayer();
     }
 }
